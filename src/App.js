3f394072a96dad1746900cb1cfda23dd89cb2f85
XNAL <772528797@qq.com>
2018-03-22 22:48:24 +0800

    Header and NavBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Listen Music</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBar/>
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <ul className="nav-bar-list">
+        <li className="nav-bar-item active">推荐</li>
+        <li className="nav-bar-item">歌手</li>
+        <li className="nav-bar-item">排行榜</li>
+      </ul>
+    );
+  }
+}
