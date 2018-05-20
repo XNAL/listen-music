ab5b928dbf65ee017bff1ff5189ae0295f4084f4
XNAL <772528797@qq.com>
2018-05-20 22:49:49 +0800

    搜索结果的处理

--- src/view/Search/Search.js
+++ src/view/Search/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './Search.scss'
 import fetch from '../../fetch/index'
 import storage from '../../util/storage'
@@ -12,7 +13,12 @@ export default class Search extends Component {
       hotKeys: [],
       searchHistory: [],
       special_key: '',
-      special_url: ''
+      special_url: '',
+      searchResult: [],
+      viewPortHeight: document.documentElement.clientHeight - 142,
+      page: 1,
+      isEnd: false,
+      isLoading: false
     }
     this.cancelInput = this.cancelInput.bind(this)
     this.inputOnFocus = this.inputOnFocus.bind(this)
@@ -20,9 +26,13 @@ export default class Search extends Component {
     this.search = this.search.bind(this)
     this.deleteSearchHistory = this.deleteSearchHistory.bind(this)
     this.clearSearchHistory = this.clearSearchHistory.bind(this)
+    this.handleSinger = this.handleSinger.bind(this)
+    this.handleScroll = this.handleScroll.bind(this)
+    this.handleClickSearch = this.handleClickSearch.bind(this)
   }
 
   componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
     fetch.getHotKey()
       .then(res => {
         this.setState({
@@ -38,11 +48,65 @@ export default class Search extends Component {
   }
 
   componentWillUnmount () {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
 
+  handleScroll (event) {
+    let scrollTop = document.body.scrollTop | document.documentElement.scrollTop
+    if (this.refs.resultRef) {
+      let listHeight = this.refs.resultRef.clientHeight
+      if ((listHeight - this.state.viewPortHeight - scrollTop <= 100) && !this.state.isEnd && !this.state.isLoading) {
+        let page = this.state.page
+        page ++
+        this.setState({
+          isLoading: true,
+          page: page
+        }, () => {
+          this.fetchSearch()
+        })
+      }
+    }
+  }
+  
+  fetchSearch () {
+    fetch.searchByKey(this.state.searchKey, this.state.page)
+      .then(res => {
+        let isEnd = false
+        let resultSong = res.data.song
+        let searchResult = this.state.searchResult
+        if (this.state.page === 1 && res.data.zhida && (res.data.zhida.type === 2 || res.data.zhida.type === 3) ) {
+          searchResult.push(Object.assign(res.data.zhida, {
+            isLink: true
+          }))
+        }
+        if (resultSong.list && resultSong.list.length > 0) {
+          searchResult = searchResult.concat(resultSong.list)
+        }
+        if (resultSong.curnum * resultSong.curpage >= resultSong.totalnum) {
+          isEnd = true
+        }
+        this.setState({
+          isLoading: false,
+          isEnd: isEnd,
+          searchResult: searchResult
+        })
+      })
+  }
+
+  search (e) {
+    if (e.key === 'Enter') {
+      this.setSearchHistory()
+      this.setState({
+        page: 1
+      }, () => {
+        this.fetchSearch()
+      })
+    }
   }
 
   cancelInput () {
     this.setState({
+      searchResult: [],
       searchKey: '',
       focus: false
     })
@@ -60,21 +124,8 @@ export default class Search extends Component {
     })
   }
 
-  search (e) {
-    if (e.key === 'Enter') {
-      let searchKey = this.state.searchKey
-      this.setState({
-        searchKey: ''
-      })
-      this.setSearchHistory(searchKey)
-      fetch.searchByKey(searchKey, this.state.page)
-        .then(res => {
-          console.log('res', res)
-        })
-    }
-  }
-
-  setSearchHistory (searchKey) {
+  setSearchHistory () {
+    let searchKey = this.state.searchKey
     let searchList = this.state.searchHistory
     let index = searchList.findIndex((value) => {
       return value === searchKey
@@ -89,7 +140,21 @@ export default class Search extends Component {
     storage.setSearchHistory(searchList)
   }
 
-  deleteSearchHistory (index) {
+  handleClickSearch (keyword) {
+    this.setState({
+      searchResult: [],
+      searchKey: keyword,
+      focus: true,
+      isLoading: true,
+      page: 1
+    }, () => {
+      this.setSearchHistory()
+      this.fetchSearch()
+    })
+  }
+
+  deleteSearchHistory (e, index) {
+    e.stopPropagation()
     let searchList = this.state.searchHistory
     searchList.splice(index, 1)
     this.setState({
@@ -105,7 +170,54 @@ export default class Search extends Component {
     storage.setSearchHistory([])
   }
 
+  // 处理歌手数据
+  handleSinger(singers) {
+    let formatSingers = []
+    for (let singer of Object.values(singers)) {
+      formatSingers.push(singer.name)
+    }
+    return formatSingers.join(' / ')
+  }
+
   render() {
+    let searchResultArray = []
+    this.state.searchResult.forEach((result, index) => {
+      if (result.isLink) {
+        if (result.type === 2) {
+          searchResultArray.push(
+            <li className="search-result-item" key={index}>
+              <Link to={`/SingerInfo/${result.singerid}`} className="singer-jump">
+              <img className="singer-img" src={`https://y.gtimg.cn/music/photo_new/T001R68x68M000${result.singermid}.jpg?max_age=2592000`}></img>
+                <p className="search-result-item-name">{result.singername}</p>
+                <p className="data-list">
+                   <span className="data-item">单曲：{result.songnum}</span>
+                   <span className="data-item">专辑：{result.albumnum}</span>
+                </p>
+              </Link>
+            </li>
+          )
+        } else if (result.type === 3) {
+          searchResultArray.push(
+            <li className="search-result-item" key={index}>
+              <Link to={`/AlbumInfo/${result.albummid}`} className="singer-jump">
+              <img className="album-img" src={`https://y.gtimg.cn/music/photo_new/T002R68x68M000${result.albummid}.jpg?max_age=2592000`}></img>
+                <p className="search-result-item-name">{result.albumname}</p>
+                <p className="search-result-item-singer">{result.singername}</p>
+              </Link>
+            </li>
+          )
+        }
+      } else {
+        console.log(result)
+        searchResultArray.push(
+          <li className="search-result-item" key={index}>
+            <i className="iconfont icon-music" />
+            <p className="search-result-item-name">{result.songname}</p>
+            <p className="search-result-item-singer">{this.handleSinger(result.singer)}</p>
+          </li>
+        )
+      }
+    })
     return (
       <section className="search-section">
         <div className="search-input-container">
@@ -130,20 +242,20 @@ export default class Search extends Component {
           }
           {
             this.state.hotKeys.map((key, index) => (
-              <div className="hot-key-item" key={index}>{key.k}</div>
+              <div className="hot-key-item" key={index}  onClick={() => this.handleClickSearch(key.k)}>{key.k}</div>
             ))
           }
         </div>
         {
-          this.state.focus &&
+          !this.state.isLoading && this.state.focus &&
           <div className="search-history-container">
             <ul className="search-history-list">
               {
-                this.state.searchHistory.map((key, index) => (
-                  <li className="search-history-item" key={index}>
+                this.state.searchHistory.map((result, index) => (
+                  <li className="search-history-item" key={index} onClick={() => this.handleClickSearch(result)}>
                     <i className="iconfont icon-history" />
-                    <span className="history-key">{key}</span>
-                    <i className="iconfont icon-delete" onClick={(index) => this.deleteSearchHistory(index)}/>
+                    <span className="history-key">{result}</span>
+                    <i className="iconfont icon-delete" onClick={(e) => this.deleteSearchHistory(e, index)}/>
                   </li>
                 ))
               }
@@ -153,6 +265,14 @@ export default class Search extends Component {
               <p className="clear-history" onClick={this.clearSearchHistory}>清除搜索记录</p>
             }
           </div>
+        }
+        {
+          this.state.focus && this.state.searchResult.length > 0 &&
+          <div className="search-reuslt-container">
+            <ul className="search-result-list" ref="resultRef">
+              { searchResultArray }
+            </ul>
+        </div>
         }
       </section>
     )
